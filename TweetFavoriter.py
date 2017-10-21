ccc7dbb33aac08becc6da3bf926a7ed81ae491b7
antoinemcgrath <antoine.mcgrath@gmail.com>
2017-10-21 11:01:03 -0700

    "TweetFavoriter backup updated: TweetFavoriter.py"

--- TweetFavoriter.py
+++ TweetFavoriter.py
@@ -36,9 +36,9 @@ print("    3. your query words variable is:", (query))
 
 
 #### Allow for reject lists (auto-lowercase for faster computing)
-auto_rejects = ['100 crs', 'TurkeyInsights', 'Crs_tiant', 'pakai', 'CAR014, Neuro', 'toxicities', 'jamesjameson331', 'GUDID',
-                'crosses', 'Judwa2,' 'BoxOffice', 'Common Reporting Standard', 'CAR-T', 'lac crs', 'FALCON', 'ramikstalk', 'STEPSociety',
-                'ZairaWasim', 'bollywood', 'Qiita', 'Emaar', 'CRSHOA', 'CRS_HOA', '#CSR', 'potaan', 'AjEeT', 'ajakkumama9', 'kamaldaiya66',
+auto_rejects = ['100 crs', 'TurkeyInsights', 'Crs_tiant', 'pakai', 'CAR014, Neuro', 'toxicities', 'jamesjameson331', 'GUDID', 'Ramesh', 'Khatauli',
+                'crosses', 'Judwa2,' 'BoxOffice', 'Common Reporting Standard', 'CAR-T', 'lac crs', 'FALCON', 'ramikstalk', 'STEPSociety', 'Golmaal',
+                'ZairaWasim', 'bollywood', 'Qiita', 'Emaar', 'CRSHOA', 'CRS_HOA', '#CSR', 'potaan', 'AjEeT', 'ajakkumama9', 'kamaldaiya66', 'CbC',
                 'Speechlys', 'CRS_Healthcare', 'pharmac', 'csr', 'god', '20 crs', 'white nationalist', 'CRSHA', 'JassimShams',
                 'KKK','nazi','hitler','tea party', 'tea pain', 'tea bagger', 'racist', 'fuck', 'CRS_Expertise']
 
